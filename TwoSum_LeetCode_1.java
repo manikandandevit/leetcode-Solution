c3c6d29baebe345e338032f6ff15368f677d803e
manikandandevit <manikandan.dev.it@gmail.com>
2025-11-02 16:16:48 +0530

    TwoSum_LeetCode_1 is Solved with HashMap

---
+++ TwoSum_LeetCode_1.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer , Integer> map = new HashMap();
+        int complement;
+        for(int i=0;i<nums.length;i++){
+            complement = target - nums[i];
+            if(map.containsKey(complement)){
+                return new int[] {map.get(complement),i};
+            }
+            map.put(nums[i],i);
+        }
+        return null;
+    }
+}
+
+/*
+                               Example
+import java.util.HashMap;
+import java.util.Arrays;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int complement;
+        for (int i = 0; i < nums.length; i++) {
+            complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = sol.twoSum(nums, target);
+        System.out.println("Output: " + Arrays.toString(result));
+    }
+}
+*/
